ccf86ed02917804b0379d906a7f9f6903f3f4b66
Julio Cesar <juliocsrmf@gmail.com>
2021-04-09 02:33:43 -0300

    ativando body parser

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.set('view engine', 'ejs'); // Definindo o motor de HTML como EJS
 app.use(express.static('public')); // Definindo pastas de arquivos estatícos
+app.use(express.urlencoded({extended: true})); // Ativando o body-parser
+app.use(express.json()); // Ativando leitura de dados em JSON
 
 app.get('/', (req, res) => {
 	res.render('index'); // Renderizando minha view
@@ -13,7 +15,10 @@ app.get('/perguntar', (req, res) => {
 });
 
 app.post('/salvarpergunta', (req, res) => {
-	res.send('Formularío enviado!');
+	let title = req.body.titulo;
+	let description = req.body.descricao;
+
+	res.send(`Título: ${title} / Descrição: ${description}`);
 });
 
 app.listen(8080, () => {
