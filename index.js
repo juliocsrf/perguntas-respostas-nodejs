7279056fc38c5f83ba88d310acb2070337299bcf
Julio Cesar <juliocsrmf@gmail.com>
2021-04-09 02:27:47 -0300

    basic template

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res)=>{
-	res.send('Hello world!');
+app.set('view engine', 'ejs'); // Definindo o motor de HTML como EJS
+app.use(express.static('public')); // Definindo pastas de arquivos estatícos
+
+app.get('/', (req, res) => {
+	res.render('index'); // Renderizando minha view
 });
 
-app.listen(8080, ()=>{
+app.get('/perguntar', (req, res) => {
+	res.render('perguntar');
+});
+
+app.post('/salvarpergunta', (req, res) => {
+	res.send('Formularío enviado!');
+});
+
+app.listen(8080, () => {
 	console.log('Servidor iniciado...');
 });
